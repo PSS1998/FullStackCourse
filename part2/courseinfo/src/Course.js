e8331a34d31015f2f4598a056fad2c82e22fc694
Parsa <pss@DESKTOP-BTDBQT4>
2022-10-16 23:35:20 +0300

    Added courseinfo

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,25 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part} />)}  
+  </>
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total sum={props.course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+    </div>
+  )
+}
+
+export default Course
